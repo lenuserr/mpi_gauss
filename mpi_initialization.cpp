572414911968b1be8a6e90c57b1c7ad69466988d
lenuser <alexey.ryndich@yandex.ru>
2024-03-21 15:58:48 +0300

    эмиграция на столбцы

--- mpi_initialization.cpp
+++ mpi_initialization.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <string>
 #include <cmath>
+// а за ноут сел так-то вообще в 11 00.
 // 11:45. Переписываю, чтобы разделение стало по столбцам.
 // Весь день есть кодить, максимум Димон наберет и усё, ваще не проблема.
 // А самые оптимальные звонки теперь это в поездках в автобусе, реально, супер оптимизация.
@@ -21,19 +22,19 @@ int g2l(int n, int m, int p, int i_glob) {
 }
 
 // максимальное число блочных строк на процесс
-int get_max_rows(int n, int m, int p) {
+int get_max_cols(int n, int m, int p) {
     int b = (n + m - 1) / m;
     return (b + p - 1) / p;
 }
 
-// число строк на процесс k
-int get_rows(int n, int m, int p, int k) {
-    // число блочных строк
+// число столбцов на процесс k
+int get_cols(int n, int m, int p, int k) {
+    // число блочных столбцов
     int b = (n + m - 1) / m;
     return (b % p) >= k + 1 ? (b / p) + 1 : b / p; 
 }
 
-// в каком процессе лежит строка i_glob
+// в каком процессе лежит столбец i_glob
 int get_k(int n, int m, int p, int i_glob) {
     int i_glob_m = i_glob / m;
     return i_glob_m % p;
@@ -55,16 +56,16 @@ double f(int s, int n, int i, int j) {
 }
 
 void init_matrix(double* a, /* своя часть */ int n, int m, int p, int k, int s, double (*f)(int, int, int, int)) {
-    int rows = get_rows(n, m, p, k); // число блочных строк в процессе k
-
-    for (int i_loc_m = 0; i_loc_m < rows; ++i_loc_m) {
-        for (int i_loc = i_loc_m * m; i_loc < (i_loc_m + 1) * m; ++i_loc) {
-            int i_glob = l2g(n, m, p, k, i_loc);
-            if (i_glob >= n) {
-                break;
-            }
-            for (int j = 0; j < n; ++j) {
-                a[n*i_loc + j] = f(s, n, i_glob, j);
+    int cols = get_cols(n, m, p, k); // число блочных столбцов на процесс k
+    for (int i_loc_m = 0; i_loc_m < cols; ++i_loc_m) {
+        for (int j = 0; j < n; ++j) {
+            for (int i_loc = i_loc_m * m; i_loc < (i_loc_m + 1) * m; ++i_loc) {
+                int i_glob = l2g(n, m, p, k, i_loc);
+                if (i_glob >= n) {
+                    break; 
+                }
+                
+                a[cols*m*j + i_loc] = f(s, n, j, i_glob);
             }
         }
     }
@@ -83,7 +84,7 @@ int read_array(FILE* fp, double* buf, int len) {
 // читает 1 процесс и рассылает остальным, читает в буфер
 // в остальных процессах есть буфер для получения
 int read_matrix(double* a, int n, int m, int p, int k, 
-    const char* name, double* buf /* буфер m x n на блочную строку */, MPI_Comm com) {
+    const char* name, double* buf /* буфер n x m на блочный столбец */, MPI_Comm com) {
     
     int main_k = 0;
     FILE* fp = nullptr;
@@ -97,20 +98,20 @@ int read_matrix(double* a, int n, int m, int p, int k,
     // в процессе main_k файл открыт
     memset(buf, 0, n*m*sizeof(double));
 
-    // число блочных строк 
+    // число блочных столбцов
     int max_b = (n + m - 1) / m;
     for (int b = 0; b < max_b; ++b) {
-        // цикл по блочным строкам в каждом процессе идет
-        // owner of row
+        // цикл по блочным столбцам в каждом процессе идет
+        // owner of col
         int owner = b % p;
         int num_rows = ((b + 1) * m  <= n ? m : n - b * m);
-        // локальный номер блочной строки
+        // локальный номер блочного столбца
         int b_loc = b / p;
         if (k == main_k) {
             err += read_array(fp, buf, n*num_rows);
             if (owner == k) {
                 // копируем на место 
-                memcpy(a + b_loc*m*n, buf, n*num_rows*sizeof(double)); // ТУТ ВРОДЕ ДИЧЬ НАПИСАНА ПРО B_LOC
+                memcpy(a + b_loc*m*n, buf, n*num_rows*sizeof(double)); 
             } else {
                 // отправляем получателю
                 MPI_Send(buf, n*num_rows, MPI_DOUBLE, owner, 0 /*tag*/, com);
@@ -141,15 +142,15 @@ void print_matrix(double* a, int n, int m, int p, int k,
     int max_b = (n + m - 1) / m;
     int printed_rows = 0;
     for (int b = 0; b < max_b; ++b) {
-        // owner of row
+        // owner of col
         int owner = b % p;
-        // num of rows
+        // num of cols
         int num_rows = (b + 1) * m <= n ? m : n - b * m; 
-        // loc num of row
+        // loc num of col
         int b_loc = b / p;
         if (k == main_k) {
             if (owner == main_k) {
-                // строка у главного процесса
+                // столбец у главного процесса
                 printed_rows += print_array(a + b_loc*m*n, n, num_rows, printed_rows, max_print);
             } else {
                 MPI_Status st;
@@ -195,7 +196,6 @@ int main(int argc, char* argv[]) {
     
     initialization(argc, argv, com, p, k); 
     MPI_Finalize();
-    
     return 0;
 }
 
@@ -205,10 +205,10 @@ void initialization(int argc, char* argv[], MPI_Comm com, int p, int k) {
     int r = std::stoi(argv[3]);
     int s = std::stoi(argv[4]);
     
-    int rows = get_rows(n, m, p, k);
+    int cols = get_cols(n, m, p, k);
     int l = n % m;
-    double* a = new double[rows*m*n];
-    double* buf = new double[m*n];
+    double* a = new double[n*cols*m];
+    double* buf = new double[n*m];
     
     if (s) {
         init_matrix(a, /* своя часть */ n, m, p, k, s, &f);
@@ -216,8 +216,7 @@ void initialization(int argc, char* argv[], MPI_Comm com, int p, int k) {
         const char* name_file = argv[argc - 1];
         read_matrix(a, n, m, p, k, name_file, buf /* буфер m x n на блочную строку */, com);
     }
-
-    print_matrix(a, n, m, p, k, buf, r, com);
+    
 
     delete[] a;
     delete[] buf;
